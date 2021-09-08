2602a1ee6d4ffbea636efbfd148cdd3eaf5f4061
linhnguyen125 <linhnguyennd125@gmail.com>
2021-09-08 17:48:10 +0700

    linh tinh

--- index.php
+++ index.php
@@ -3,11 +3,22 @@ require('./inc/header.php');
 ?>
 <div id="wrapper-content">
     <div class="row">
-        <?php  require('./inc/side_bar.php') ?>
+        <?php require('./inc/side_bar.php') ?>
         <div class="col-md-9">
             <div id="content">
                 <p class="bt">Bài làm</p>
                 <hr />
+                <div class="ex">
+                    <p style="font-size: 18px; font-weight: 500;">Nếu các người thành tâm muốn biết, thì chúng tôi sẵn lòng trả lời.<br />
+                        Để đề phòng thế giới bị phá hoại, để bảo vệ nền hòa bình thế giới.<br />
+                        Chúng tôi đại diện cho những nhân vật phản diện, đầy khả ái và ngây ngất lòng người.<br />
+                        Musha - Kochiro<br />
+                        Chúng tôi là Đội hỏa tiễn,<br />
+                        Bảo vệ dải ngân hà,<br />
+                        Một tương lai tươi sáng đang chờ đợi chúng tôi.<br /><br />
+                        Chuyện là thế đấy
+                        Meo ~~~</p>
+                </div>
             </div>
         </div>
     </div>
